399722d8e6226bbb63ce829aea4ebcc9cc234a77
chiafeng79 <chiafeng79@gmail.com>
2019-11-27 17:11:59 +0800

    ver 1.0

--- app/src/main/java/com/sample/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/sample/tictactoeapp/MainActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,6 +34,8 @@ class MainActivity : AppCompatActivity() {
         Log.d("btnClicked", cellID.toString() )
 
         playGame(cellID, btnSelected)
+
+        autoPlay()
     }
 
     var activePlayer = 1
@@ -58,56 +62,48 @@ class MainActivity : AppCompatActivity() {
 
     fun checkWinner(){
         var winner = -1
-
         //row
-        if (player1.contains(1)&&player1.contains(2)&&player1.contains(3)){
+        if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
             winner = 1
         }
-        if (player2.contains(1)&&player2.contains(2)&&player2.contains(3)){
+        if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
             winner = 2
         }
-
         //row
-        if (player1.contains(4)&&player1.contains(5)&&player1.contains(6)){
+        if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
             winner = 1
         }
-        if (player2.contains(4)&&player2.contains(5)&&player2.contains(6)){
+        if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
             winner = 2
         }
-
         //row
-        if (player1.contains(7)&&player1.contains(8)&&player1.contains(9)){
+        if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
             winner = 1
         }
-        if (player2.contains(7)&&player2.contains(8)&&player2.contains(9)){
+        if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
             winner = 2
         }
-
-
         //cloum
-        if (player1.contains(1)&&player1.contains(4)&&player1.contains(7)){
+        if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
             winner = 1
         }
-        if (player2.contains(1)&&player2.contains(4)&&player2.contains(7)){
+        if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
             winner = 2
         }
-
         //cloum
-        if (player1.contains(2)&&player1.contains(5)&&player1.contains(8)){
+        if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
             winner = 1
         }
-        if (player2.contains(2)&&player2.contains(5)&&player2.contains(8)){
+        if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
             winner = 2
         }
-
         //cloum
-        if (player1.contains(3)&&player1.contains(6)&&player1.contains(9)){
+        if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
             winner = 1
         }
-        if (player2.contains(3)&&player2.contains(6)&&player2.contains(9)){
+        if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
             winner = 2
         }
-
         //sp
         if (player1.contains(1) && player1.contains(5) && player1.contains(9)) {
             winner = 1
@@ -115,7 +111,6 @@ class MainActivity : AppCompatActivity() {
         if (player2.contains(1) && player2.contains(5) && player2.contains(9)) {
             winner = 2
         }
-
         //sp
         if (player1.contains(3) && player1.contains(5) && player1.contains(7)) {
             winner = 1
@@ -126,12 +121,79 @@ class MainActivity : AppCompatActivity() {
 
 
         if (winner == 1){
+            player1Count += 1
             Toast.makeText(this, "Player 1 win the game", Toast.LENGTH_LONG).show()
+            restartGame()
+
         }else if(winner == 2){
             Toast.makeText(this, "Player 2 win the game", Toast.LENGTH_LONG).show()
+            player2Count += 1
+            restartGame()
+        }
+    }
+
+    fun autoPlay(){
+        var emptyCell = ArrayList<Int>()
+        for (cellId in  1..9){
+            if (!(player1.contains(cellId)||player2.contains(cellId))){
+                emptyCell.add(cellId)
+            }
+        }
+        val r = Random
+        val rIndex = r.nextInt(emptyCell.size)
+        val cellId = emptyCell[rIndex]
+        var btnSelected: Button?
+        btnSelected = when(cellId) {
+            1 -> bnt1
+            2 -> bnt2
+            3 -> bnt3
+            4 -> bnt4
+            5 -> bnt5
+            6 -> bnt6
+            7 -> bnt7
+            8 -> bnt8
+            9 -> bnt9
+            else -> {
+                bnt1
+            }
+        }
+        playGame(cellId, btnSelected)
+
+        if (emptyCell.size == 0){
+            restartGame()
+        }
+    }
+
+
+    var player1Count = 0
+    var player2Count = 0
+    fun restartGame(){
+        activePlayer = 1
+        player1.clear()
+        player2.clear()
+        for (cellId in 1..9){
+            var btnSelected: Button?
+            btnSelected = when(cellId) {
+                1 -> bnt1
+                2 -> bnt2
+                3 -> bnt3
+                4 -> bnt4
+                5 -> bnt5
+                6 -> bnt6
+                7 -> bnt7
+                8 -> bnt8
+                9 -> bnt9
+                else -> {
+                    bnt1
+                }
+            }
+            btnSelected!!.text = ""
+            btnSelected!!.setBackgroundResource(R.color.btn)
+            btnSelected!!.isEnabled = true
         }
 
-
+        Toast.makeText(this, "Player1: $player1Count \t Player2: $player2Count", Toast.LENGTH_LONG).show()
 
     }
+
 }
